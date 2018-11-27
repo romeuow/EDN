7e6d257a94eb66ad16ea127158ea6cd36357fdc7
Romeu <romeu.oliveira@powerlogic.com.r>
2018-11-27 09:21:13 -0200

    F.Euler

--- Trabalho 1/src/Solver.py
+++ Trabalho 1/src/Solver.py
@@ -11,10 +11,27 @@ class SolverClass:
 		while yPrevious is None or np.max(abs(yPrevious[:,0:errorSize] - yv[:,0:errorSize])) > problem.Eh :
 			n *= 2
 			yPrevious = yv
-			t, yv = self.RK4(problem, b, n)
+			# t, yv = self.RK4(problem, b, n)
+			t, yv = self.ForwardEuler(problem, b, n)
 			
 		return t, yv
-		
+
+
+	def ForwardEuler(self, problem, b, n):
+		dt = (problem.t-problem.t0)/float(n)
+		t = np.zeros(n+1)
+		yv = np.zeros((2, n+1))
+		yv[0][0] = problem.y0
+		yv[1][0] = problem.v0
+		t[0] = problem.t0
+		for k in range(n):
+			t[k+1] = t[k] + dt
+			yv[:,k+1] = yv[:,k] + np.multiply(dt, problem.function(t[k], yv[:,k], b))
+			
+		return t, yv
+
+
+
 	def RK4(self, problem, b, n):
 		dt = (problem.t-problem.t0)/float(n)
 		t = np.zeros(n+1)
