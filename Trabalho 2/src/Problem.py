49a4d4297cfac27d56a05ad7c8768aad4705329c
Romeu <romeu.oliveira@powerlogic.com.r>
2018-12-03 09:23:15 -0200

    Fix this

--- Trabalho 2/src/Problem.py
+++ Trabalho 2/src/Problem.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import integrate
 
 class ProblemClass:	
 	
@@ -17,5 +18,24 @@ class ProblemClass:
 		else:
 			return 0		
 
+	def exact_solution(self, x, t):
+
+		def integrand(x, a, n):
+
+			if (a >= np.pi/4) and (a <= np.pi/2):
+				return (4/np.pi)*(x - (np.pi/4))*np.sin(n*np.pi/self.L*x)
+			else:
+				return (4/np.pi)*((3*np.pi/4) - x)*np.sin(n*np.pi/self.L*x)
+
+		exact = 0
+		for n in range(1,3)
+			bn = 2/self.L*integrate.quad(integrand, np.pi/4, np.pi/2, args=(np.pi/2, n))[0] + integrate.quad(integrand, np.pi/2, 3*np.pi/4, args=(3*np.pi/4, n))[0]
+			exact += bn * (np.exp(-(n*np.pi/self.L)**2))*problem.k*t*np.sin(n*np.pi/self.L*x)
+		exact += 
+
+		print(bn)
+
+
 	def N(self):
 		return int(np.ceil(self.T * self.k * (self.M ** 2) * (1 / (self.lbda * (self.L ** 2)))))
+
